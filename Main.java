f54a8ffc925506680c6f48da3a44ffda25aea665
Aalisha2004 <aalishapatwekar001@gmail.com>
2024-08-17 15:33:51 +0530

    Add Task 3 - ATM Machine implementation

---
+++ Main.java
@@ -0,0 +1,13 @@
+package ATMProject.src;
+
+public class Main{
+
+    public static void main(String args[]){
+
+        BankAccount account=new BankAccount(1000);
+
+        ATM atm=new ATM(account);
+
+        atm.start();
+    }
+}
